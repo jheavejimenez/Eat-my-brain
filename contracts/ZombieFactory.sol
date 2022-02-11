edcb5fae7a1a955d356aec9281c80a00201056e2
Jheave <jimenezjheave123@gmail.com>
2022-02-12 00:17:11 +0800

    Added a helper function for generating DNA

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -16,6 +16,9 @@ contract ZombieFactory {
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
     } 
-    
-    //TODO: create a helper that generates a random DNA
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
 }
