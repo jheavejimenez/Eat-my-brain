4fab059b0a1bab2b2d5d8f875d38e274494bbc23
Jheave <jimenezjheave123@gmail.com>
2022-02-13 10:10:29 +0800

    created a mapping for keep tracking

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -15,6 +15,9 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
         uint id = zombies.length  - 1;
