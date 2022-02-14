95daf034def7ebdfa2703c8c2761d4137fcd6ea7
Jheave <jimenezjheave123@gmail.com>
2022-02-14 23:27:53 +0800

    created new contract

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -4,5 +4,8 @@ pragma solidity ^0.8.4;
 import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+    }
 }
